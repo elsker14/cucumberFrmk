df2d29a53f30996bdf6a0472f48c7301c9bac142
Iancu Jianu <iancujianu98@gmail.com>
2024-03-13 20:59:03 +0200

    last math operations

--- src/main/java/org/example/step_definitions/CalculatorSteps.java
+++ src/main/java/org/example/step_definitions/CalculatorSteps.java
@@ -11,7 +11,7 @@ public class CalculatorSteps {
     public float actualResult;
 
     @Given("I have a calculator")
-    public void iHaveACalculator(){
+    public void iHaveACalculator() {
         this.myCalculator = new Calculator("stiintific");
     }
 
@@ -20,9 +20,24 @@ public class CalculatorSteps {
         this.actualResult = this.myCalculator.sum(a, b);
     }
 
+    @When("I subtract {float} from {float}")
+    public void iSubtract(float a, float b) {
+        this.actualResult = this.myCalculator.subtract(a, b);
+    }
+
+    @When("I multiply {float} times by {float}")
+    public void iMultiply(float a, float b) {
+        this.actualResult = this.myCalculator.sum(a, b);
+    }
+
+    @When("I divide {float} to {float}")
+    public void iDivide(float a, float b) {
+        this.actualResult = this.myCalculator.sum(a, b);
+    }
+
     @Then("I expect the result to be {float}")
-    public void iExpectTheResultToBe(float expectedResult){
-        if(this.actualResult == expectedResult) {
+    public void iExpectTheResultToBe(float expectedResult) {
+        if (this.actualResult == expectedResult) {
             System.out.println("Test passed!");   // sout
         } else {
             System.out.println("Test failed!");
@@ -30,7 +45,5 @@ public class CalculatorSteps {
 
         Calculator.suntFloricel();
     }
-
-
 
 }
